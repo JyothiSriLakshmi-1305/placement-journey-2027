d3ff91efdfbe9943d6fcb8883528ea1d808e53a2
Jyothi Sri Lakshmi <jyothikuna2005@gmail.com>
2026-07-10 16:08:05 +0530

    Add Java Operator notes

---
+++ java/operators/TernaryOperator.java
@@ -0,0 +1,12 @@
+
+class TernaryOperator{
+    public static void main(String args[]){
+
+
+int number = -5;
+
+String result = (number >= 0) ? "Positive" : "Negative";
+
+System.out.println(result);
+    }
+}
