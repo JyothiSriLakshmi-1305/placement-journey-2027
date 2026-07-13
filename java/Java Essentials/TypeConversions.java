c4b3d6e25fb883a60dbb072c053cfb96ad987769
Jyothi Sri Lakshmi <jyothikuna2005@gmail.com>
2026-07-13 10:07:37 +0530

    Add Java Type Conversion  notes

---
+++ java/Java Essentials/TypeConversions.java
@@ -0,0 +1,10 @@
+class TypeConversions{
+    public static void main(String args[]){
+        //int x=5.99; error:Incompatible types
+        //System.out.println(x);
+        int x=5;
+        double y=x;
+        System.out.println(y);
+
+    }
+}
