fd9a5a34eb50194e8cd72377b7e674c73b796493
Jyothi Sri Lakshmi <jyothikuna2005@gmail.com>
2026-07-28 12:28:09 +0530

    Break example program .

---
+++ java/Phase 5 -loops/programs/Break.java
@@ -0,0 +1,26 @@
+class Break{
+    public static void main(String args[]){
+        for(int i = 1; i <= 10; i++){
+
+    if(i == 5){
+        break;
+    }
+
+    System.out.println(i);
+
+}
+System.out.println("another break usecase observe clearly output");
+
+//another observe clearly output
+for(int i = 1; i <= 10; i++){
+
+    System.out.println(i);
+
+    if(i == 5){
+        break;
+    }
+
+}
+
+    }
+}
