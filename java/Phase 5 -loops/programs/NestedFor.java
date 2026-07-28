b493a9339cf13f1979ab5ed95b69283a851bc9e8
Jyothi Sri Lakshmi <jyothikuna2005@gmail.com>
2026-07-28 10:37:33 +0530

    Add the NestedForLoop program.

---
+++ java/Phase 5 -loops/programs/NestedFor.java
@@ -0,0 +1,30 @@
+class NestedFor{
+    public static void main(String args[]){
+        for(int i = 1; i <= 2; i++){
+        for(int j = 1; j <= 3; j++){
+            System.out.print("* ");
+            }
+            System.out.println();
+        }
+        //pattern 2
+        for(int i = 1; i <= 2; i++){
+        for(int j = 1; j <= 3; j++){
+            System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i <= 4; i++){
+
+            for(int j = 1; j <= i; j++){
+
+                System.out.print(i + " ");
+
+        }
+
+    System.out.println();
+
+}
+
+    }
+}
