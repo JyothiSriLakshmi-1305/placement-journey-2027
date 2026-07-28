d223f649b4f82c7d57a512c434abcb06eb3d16b3
Jyothi Sri Lakshmi <jyothikuna2005@gmail.com>
2026-07-28 11:26:26 +0530

    Add the NestedForLoop program.

---
+++ java/Phase 5 -loops/programs/AdvancedNested.java
@@ -0,0 +1,15 @@
+class AdvancedNested{
+    public static void main(String args[]){
+        for(int table = 1; table <= 3; table++){ //which table?
+
+    for(int i = 1; i <= 10; i++){ //mutliply by upto 1 to 10
+
+        System.out.println(table + " x " + i + " = " + table*i);
+
+    }
+
+    System.out.println();
+
+}
+    }
+}
