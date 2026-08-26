f677d60792d73c83f7ddabda01422962724cf174
Jyothi Sri Lakshmi <jyothikuna2005@gmail.com>
2026-08-26 21:50:07 +0530

    Added the loop program example.

---
+++ java/Phase 5 -loops/programs/Codechef.java
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+        // For loop iterates from 1 to 5
+        for (int i = 1; i <= 5; i++);{
+            //System.out.println(i); // Print the current value of i
+        }
+    }
+}
